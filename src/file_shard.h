60be422485fccadbe6317bf7bbba406e4f5f6785
Bill Z. Xing <bill.z.xing@gmail.com>
2018-03-30 17:58:21 -0400

    File sharding

--- src/file_shard.h
+++ src/file_shard.h
@@ -60,6 +60,90 @@ namespace
 		os << oss.str() << std::flush;
 	}
 
+	unsigned determine_shard_length_simple(
+		  unsigned target_shard_length
+		, unsigned remaining_file_length
+		)
+	{
+		unsigned result = [&]()
+		{
+			// If remaining is less than 1.5x of target shard length, use the remaining length
+			if (  remaining_file_length < ( target_shard_length + (target_shard_length / 2) )  )
+			{
+				return remaining_file_length;
+			}
+
+			// Else, use the target shard length
+			return target_shard_length;
+		}();
+
+		BOOST_ASSERT(result > 0);
+		return result;
+	}
+
+	// This function try to divide shards as evenly as possible, while
+	// make sure the shard borders don't cut through a line.
+	unsigned determine_shard_length(
+		  std::ifstream & ifs
+		, const unsigned file_length
+		, const unsigned prelim_offset
+		, const unsigned target_shard_length
+		, const unsigned remaining_file_length
+		)
+	{
+		using char_type = std::ifstream::int_type;
+		char_type c = 0;
+
+		// Do a simple calculation first
+		unsigned prelim_shard_length = determine_shard_length_simple(
+			target_shard_length, remaining_file_length);
+
+		// Check if it's right at a newline or EOF character.
+		// If not, advance to the next newline or EOF character.
+		unsigned next_offset = (prelim_offset + target_shard_length);
+
+		// If shard starts from 0, use the prelim.
+		// (This should not happen)
+		if (next_offset == 0)
+		{
+			return prelim_shard_length;
+		}
+
+		// If next shard is right at EOF, use the prelim.
+		// (Next shard won't exist).
+		ifs.seekg(next_offset);
+		c = ifs.get();
+		if (ifs.eof())
+		{
+			return prelim_shard_length;
+		}
+		BOOST_ASSERT(ifs.good());
+
+
+		// Test if the end of prev shard is already a newline character.
+		// If so, nothing has to be done, use prelim.
+		ifs.seekg(next_offset - 1);
+		c = ifs.get();
+		BOOST_ASSERT(ifs.good());
+		if (c == '\n')
+		{
+			return prelim_shard_length;
+		}
+
+		// Last shard cuts through a line.
+		// To handle, get a new line by calling std::getline(), and use the new ifstream position
+		// as the new offset.
+		std::string line;
+		std::getline(ifs, line);
+		BOOST_ASSERT(ifs.good());
+
+		unsigned adjusted_offset = boost::numeric_cast<unsigned>(static_cast<long>(ifs.tellg()));
+		BOOST_ASSERT(adjusted_offset >= prelim_shard_length);
+		BOOST_ASSERT(adjusted_offset <= file_length);
+
+		return adjusted_offset - prelim_offset;
+	}
+
 	std::vector<FileShard> make_file_shards(const MapReduceSpec & mr_spec)
 	{
 		std::vector<FileShard> shard_vec;
@@ -67,40 +151,30 @@ namespace
 
 		for ( const auto & filename : mr_spec.get_input_file_range() )
 		{
-			// Iteratively split this ffile
+			// Iteratively split this file
 			const unsigned file_length = determine_file_length(filename);
-			unsigned current_offset = 0;
 
-			while (current_offset < file_length)
+			if (file_length > 0)
 			{
-				const unsigned remaining_file_length = file_length - current_offset;
+				std::ifstream ifs(filename);
+				BOOST_ASSERT_MSG(ifs.good(), "Failed open file");
 
-				// If next remaining length is less than half of target shard length,
-				// then include it in current shard
+				unsigned current_offset = 0;
 
-				auto func_determine_shard_length = [](
-					  unsigned target_shard_length
-					, unsigned remaining_file_length
-					)
+				while (current_offset < file_length)
 				{
-					// If remaining is less than 1.5x of target shard length, use the remaining length
-					if (  remaining_file_length < ( target_shard_length + (target_shard_length / 2) )  )
-					{
-						return remaining_file_length;
-					}
+					const unsigned remaining_file_length = file_length - current_offset;
 
-					// Else, use the target shard length
-					return target_shard_length;
-				};
+					const unsigned actual_shard_length = determine_shard_length(
+						ifs, file_length, current_offset,
+						target_shard_length, remaining_file_length);
 
-				const unsigned actual_shard_length = func_determine_shard_length(
-					target_shard_length, remaining_file_length);
+					BOOST_ASSERT_MSG(current_offset + actual_shard_length <= file_length,
+						"Shard range overshooting file length!");
+					shard_vec.push_back({filename, current_offset, actual_shard_length, file_length});
 
-				BOOST_ASSERT_MSG(current_offset + actual_shard_length <= file_length,
-					"Shard range overshooting file length!");
-				shard_vec.push_back({filename, current_offset, actual_shard_length, file_length});
-
-				current_offset += actual_shard_length;
+					current_offset += actual_shard_length;
+				}
 			}
 		}
 
