dba5c16d2f88001ff27860bfce595c947a059f0c
Bill Z. Xing <bill.z.xing@gmail.com>
2018-04-01 22:07:30 -0400

    Executor actuall doing some wor

--- src/worker.h
+++ src/worker.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <thread>
+#include <mutex>
+#include <condition_variable>
 #include <list>
 #include <memory>
 #include <utility>
@@ -17,16 +19,14 @@
 #include "mr_tasks.h"
 
 
-using masterworker::TaskAck;
-using masterworker::TaskRequest;
-
-
 std::shared_ptr<BaseMapper> get_mapper_from_task_factory(const std::string& user_id);
 std::shared_ptr<BaseReducer> get_reducer_from_task_factory(const std::string& user_id);
 
 
 namespace
 {
+    using ScopedLock = std::unique_lock<std::mutex>;
+
     static constexpr auto kClockType = GPR_CLOCK_MONOTONIC;
     static constexpr int kSendReplyTimeout = 5;
     static constexpr int kServerCqTimeout = 2;
@@ -54,122 +54,207 @@ namespace
     }
 }
 
+class CallData
+{
+public:
+    CallData(
+        masterworker::WorkerService::AsyncService & service
+      , grpc::ServerCompletionQueue & cq
+    ) :
+        m_responder(&m_context)
+    {
+        m_request.set_task_uid( std::numeric_limits<unsigned>::max() );
+        request_call(service, cq);
+    }
 
+    void send_reply(bool success)
+    {
+        BOOST_ASSERT(!m_send_reply_time);
+        m_send_reply_time = std::make_unique<gpr_timespec>(gpr_now(kClockType));
+
+        // Assemble reply message
+        m_reply = masterworker::TaskAck();
+        m_reply.set_task_uid( m_request.task_uid() );
+        m_reply.set_success(success);
+
+        // Call Finish
+        m_responder.Finish(m_reply, grpc::Status::OK, this);
+    }
+
+    void set_reply_returned()
+    {
+        m_reply_returned = true;
+
+        // const unsigned task_uuid = get_task_uuid();
+        // std::cout << "Reply returned for task " + std::to_string(task_uuid) + "\n" << std::flush;
+    }
+
+    bool has_sent_reply() const
+    {
+        return m_send_reply_time != nullptr;
+    }
+
+    bool should_destroy( const gpr_timespec & curr_time ) const
+    {
+        return m_reply_returned || send_reply_has_timed_out(curr_time);
+    }
+
+    unsigned get_task_uuid() const
+    {
+        return m_request.task_uid();
+    }
+
+private:
+
+    bool send_reply_has_timed_out( const gpr_timespec & curr_time ) const
+    {
+        if (m_send_reply_time)
+        {
+            gpr_timespec time_diff = curr_time - *m_send_reply_time;
+            if (time_diff.tv_sec >= kSendReplyTimeout)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void request_call(
+          masterworker::WorkerService::AsyncService & service
+        , grpc::ServerCompletionQueue & cq
+    )
+    {
+        service.RequestDispatchTaskToWorker(
+              &m_context
+            , &m_request
+            , &m_responder
+            , &cq
+            , &cq
+            , this
+        );
+    }
+
+    //Executor & m_executor;
+    //masterworker::WorkerService::AsyncService & m_service;
+    //grpc::ServerCompletionQueue & m_cq;
+
+    // The m_responder depends on m_context,
+    // so make m_context must appear before m_responder
+    grpc::ServerContext m_context;
+    grpc::ServerAsyncResponseWriter<masterworker::TaskAck> m_responder;
+
+    masterworker::TaskRequest m_request;
+    masterworker::TaskAck m_reply;
+
+    std::unique_ptr<gpr_timespec> m_send_reply_time;
+    bool m_reply_returned = false;
+};
 
 
 class Executor
 {
 public:
-    bool is_busy() const
+    Executor()
     {
-        return true;
+        m_executor_thread = std::thread(  [this]() { executor_thread(); }  );
+    }
+
+    // If executor is available, pass the call to the executor thread. The thread will
+    // be responsible for replying to the caller.
+    //
+    // If executor is busy, reject the call and send a reply directly.
+    void async_execute_and_reply(CallData * const call)
+    {
+        bool success = try_set_call(call);
+        if (!success)
+        {
+            std::cout << "Rejecting task " + std::to_string(call->get_task_uuid()) + "!\n" << std::flush;
+            call->send_reply(false);
+        }
     }
 
 private:
+
+    // If the executor is free, occupy it by setting m_call pointer,
+    // notify the executor thread, and return true. The executor
+    // thread will pick the call up
+    //
+    // Else return false.
+    bool try_set_call(CallData * const call)
+    {
+        BOOST_ASSERT(call != nullptr);
+        ScopedLock lock(m_call_ptr_mutex);
+
+        CallData * snapshot = m_call;
+        if (!snapshot)
+        {
+            m_call = call;
+            m_call_ptr_cv.notify_one();
+            return true;
+        }
+
+        return false;
+    }
+
+    void executor_thread()
+    {
+        for (;;)
+        {
+            CallData * call = wait_for_call();
+            std::cout << "Accepting task " + std::to_string(call->get_task_uuid()) + "!\n" << std::flush;
+
+            bool success = internal_execute_task(call);
+            call->send_reply(success);
+
+            unset_call(call);
+        }
+    }
+
+    // Wait until a call arrives, then return the
+    // arrived call pointer value.
+    CallData * wait_for_call()
+    {
+        ScopedLock lock(m_call_ptr_mutex);
+
+        while (!m_call)
+        {
+            m_call_ptr_cv.wait(lock);
+        }
+
+        return m_call;
+    }
+
+    bool internal_execute_task(CallData * call)
+    {
+        BOOST_ASSERT(call);
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        return true;
+    }
+
+
+    // Simply unset the call and assert call pointer is the expected value.
+    void unset_call(CallData * const call)
+    {
+        ScopedLock lock(m_call_ptr_mutex);
+
+        CallData * snapshot = m_call;
+        BOOST_ASSERT(snapshot == call);
+
+        m_call = nullptr;
+    }
+
+    CallData * m_call = nullptr; // Used to indicate whether is busy or not.
+    std::mutex m_call_ptr_mutex;
+    std::condition_variable m_call_ptr_cv;
+
+    std::thread m_executor_thread;
+
 };
 
 class Server
 {
-    //// Begin class CallData ////
-    class CallData
-    {
-    public:
-        CallData(
-            masterworker::WorkerService::AsyncService & service
-          , grpc::ServerCompletionQueue & cq
-        ) :
-            m_responder(&m_context)
-        {
-            m_request.set_task_uid( std::numeric_limits<unsigned>::max() );
-            request_call(service, cq);
-        }
-
-        void send_reply(bool success)
-        {
-            BOOST_ASSERT(!m_send_reply_time);
-            m_send_reply_time = std::make_unique<gpr_timespec>(gpr_now(kClockType));
-
-            // Assemble reply message
-            m_reply = TaskAck();
-            m_reply.set_task_uid( m_request.task_uid() );
-            m_reply.set_success(success);
-
-            // Call Finish
-            m_responder.Finish(m_reply, grpc::Status::OK, this);
-        }
-
-        void set_reply_returned()
-        {
-            m_reply_returned = true;
-
-            // const unsigned task_uuid = get_task_uuid();
-            // std::cout << "Reply returned for task " + std::to_string(task_uuid) + "\n" << std::flush;
-        }
-
-        bool has_sent_reply() const
-        {
-            return m_send_reply_time != nullptr;
-        }
-
-        bool should_destroy( const gpr_timespec & curr_time ) const
-        {
-            return m_reply_returned || send_reply_has_timed_out(curr_time);
-        }
-
-        unsigned get_task_uuid() const
-        {
-            return m_request.task_uid();
-        }
-
-    private:
-
-        bool send_reply_has_timed_out( const gpr_timespec & curr_time ) const
-        {
-            if (m_send_reply_time)
-            {
-                gpr_timespec time_diff = curr_time - *m_send_reply_time;
-                if (time_diff.tv_sec >= kSendReplyTimeout)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        void request_call(
-              masterworker::WorkerService::AsyncService & service
-            , grpc::ServerCompletionQueue & cq
-        )
-        {
-            service.RequestDispatchTaskToWorker(
-                  &m_context
-                , &m_request
-                , &m_responder
-                , &cq
-                , &cq
-                , this
-            );
-        }
-
-        //Executor & m_executor;
-        //masterworker::WorkerService::AsyncService & m_service;
-        //grpc::ServerCompletionQueue & m_cq;
-
-        // The m_responder depends on m_context,
-        // so make m_context must appear before m_responder
-        grpc::ServerContext m_context;
-        grpc::ServerAsyncResponseWriter<TaskAck> m_responder;
-
-        TaskRequest m_request;
-        TaskAck m_reply;
-
-        std::unique_ptr<gpr_timespec> m_send_reply_time;
-        bool m_reply_returned = false;
-    };
-    //// End class CallData ////
-
-
-
 public:
 
     Server(const std::string & addr)
@@ -237,9 +322,11 @@ public:
                         ++iter;
                     }
                 }
-
-                std::cout << "Destroyed " + std::to_string(destruction_count) + " requests. "
-                    + std::to_string(call_list.size()) + " left pending.";
+                if (destruction_count > 0)
+                {
+                    std::cout << "Destroyed " + std::to_string(destruction_count) + " requests. "
+                        + std::to_string(call_list.size()) + " left pending.\n" << std::flush;
+                }
             }
 
             // Process returned request on the completion queue
@@ -264,9 +351,7 @@ public:
                 else
                 {
                     // Case when receiving an incoming call.
-                    // We'll first sent the reply back
-                    call_data->send_reply(true); // Mock a success here
-                    std::cout << "Sent reply for task " + std::to_string(call_data->get_task_uuid()) + "\n" << std::flush;
+                    m_executor.async_execute_and_reply(call_data);
 
                     // Then invite a new call in
                     call_list.emplace_back(m_service, *m_cq);
